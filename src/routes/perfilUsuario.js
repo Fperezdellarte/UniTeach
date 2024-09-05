c2a0ef4ffcfb736231a555820d8ef855186a6b13
Fperezdellarte <fperezdellarte@gmail.com>
2024-09-05 17:34:55 -0300

    Merge branch 'features-home' into develop

---
+++ src/routes/perfilUsuario.js
@@ -0,0 +1,228 @@
+import React, { useEffect, useState } from 'react';
+import { Navbar } from '../components/navbar';
+import { API_URL } from '../auth/constans';
+import axios from 'axios';
+import { Button, Form, Container, Image } from 'react-bootstrap';
+import '../styles/PerfilUsuario.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importar Bootstrap
+
+const PerfilUsuario = () => {
+  const [formData, setFormData] = useState({
+    Name: '',
+    Username: '',
+    Mail: '',
+    Phone: '',
+    Avatar_URL: '',
+    Avatar_File: null,
+  });
+
+
+  const [nameError, setNameError] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+  const [mailError, setMailError] = useState('');
+  const [phoneError, setPhoneError] = useState('');
+
+  useEffect(() => {
+    const authData = JSON.parse(localStorage.getItem('authData'));
+
+    if (authData && authData.user) {
+      setFormData({
+        Name: authData.user.Name || '',
+        Username: authData.user.Username || '',
+        Mail: authData.user.Mail || '',
+        Phone: authData.user.Phone || '',
+        Avatar_URL: authData.user.Avatar_URL || '',
+      });
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value, type, files } = e.target;
+    if (type === 'file') {
+      setFormData({ ...formData, Avatar_File: files[0] });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+
+    // Validar nombre
+    const nameRegex = /^[a-z A-Z]+$/;
+    if (!nameRegex.test(formData.Name)) {
+      setNameError('El nombre no puede contener números ni caracteres especiales.');
+      isValid = false;
+    } else {
+      setNameError('');
+    }
+
+    // Validar nombre de usuario
+    const usernameRegex = /^[a-zA-Z0-9]+$/;
+    if (!usernameRegex.test(formData.Username)) {
+      setUsernameError('El nombre de usuario no puede contener caracteres especiales.');
+      isValid = false;
+    } else {
+      setUsernameError('');
+    }
+
+    // Validar correo electrónico
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailRegex.test(formData.Mail)) {
+      setMailError('El correo electrónico no es válido.');
+      isValid = false;
+    } else {
+      setMailError('');
+    }
+
+    // Validar teléfono
+    const phoneRegex = /^[0-9]{10}$/;
+    if (!phoneRegex.test(formData.Phone)) {
+      setPhoneError('El número debe contener solo números, con 10 dígitos.');
+      isValid = false;
+    } else {
+      setPhoneError('');
+    }
+
+    return isValid;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validateForm()) {
+      return;
+    }
+
+    const { Avatar_File, ...rest } = formData;
+
+    const formDataToSend = new FormData();
+    formDataToSend.append('Name', rest.Name);
+    formDataToSend.append('Username', rest.Username);
+    formDataToSend.append('Mail', rest.Mail);
+    formDataToSend.append('Phone', rest.Phone);
+    if (Avatar_File) {
+      formDataToSend.append('file', Avatar_File); // Append the file with the key 'file'
+    }
+
+    try {
+      const authData = JSON.parse(localStorage.getItem('authData'));
+      const response = await axios.patch(`${API_URL}/users/${authData.user.idUser}`, formDataToSend, {
+        headers: {
+          'Content-Type': 'multipart/form-data', // Important for file uploads
+          'Authorization': `Bearer ${authData.token}` // Include token if needed
+        }
+      });
+
+      if (response.status === 200) {
+        console.log('Profile updated successfully');
+        
+        const updatedUser = response.data.user;
+        localStorage.setItem('authData', JSON.stringify({ ...authData, user: updatedUser }));
+        
+        setFormData({
+          ...formData,
+          ...updatedUser,
+        });
+        
+      } else {
+        console.error('Error updating profile');
+      }
+    } catch (error) {
+      console.error('Network error:', error);
+    }
+  };
+   
+
+  return (
+    <div>
+      <Navbar />
+      <Container className="my-4">
+        <h2 className="perfilusuario-h2 mb-4">Perfil de Usuario</h2>
+        <Form onSubmit={handleSubmit} className="perfilusuario-profile-form">
+          <Form.Group controlId="formName" className="perfilusuario-form-group">
+            <Form.Control
+              type="text"
+              name="Name"
+              value={formData.Name}
+              onChange={handleChange}
+              placeholder=" " // Placeholder vacío para el efecto de etiqueta flotante
+              className={formData.Name ? 'filled' : ''}
+              required
+            />
+            <Form.Label className={formData.Name ? 'filled' : ''}>Nombre</Form.Label>
+            {nameError && <span className="text-danger">{nameError}</span>}
+          </Form.Group>
+
+          <Form.Group controlId="formUsername" className="perfilusuario-form-group">
+            <Form.Control
+              type="text"
+              name="Username"
+              value={formData.Username}
+              onChange={handleChange}
+              placeholder=" " // Placeholder vacío para el efecto de etiqueta flotante
+              className={formData.Username ? 'filled' : ''}
+              required
+            />
+            <Form.Label className={formData.Username ? 'filled' : ''}>Username</Form.Label>
+            {usernameError && <span className="text-danger">{usernameError}</span>}
+          </Form.Group>
+
+          <Form.Group controlId="formMail" className="perfilusuario-form-group">
+            <Form.Control
+              type="email"
+              name="Mail"
+              value={formData.Mail}
+              onChange={handleChange}
+              placeholder=" " // Placeholder vacío para el efecto de etiqueta flotante
+              className={formData.Mail ? 'filled' : ''}
+              required
+            />
+            <Form.Label className={formData.Mail ? 'filled' : ''}>Correo</Form.Label>
+            {mailError && <span className="text-danger">{mailError}</span>}
+          </Form.Group>
+
+          <Form.Group controlId="formPhone" className="perfilusuario-form-group">
+            <Form.Control
+              type="text"
+              name="Phone"
+              value={formData.Phone}
+              onChange={handleChange}
+              placeholder=" " // Placeholder vacío para el efecto de etiqueta flotante
+              className={formData.Phone ? 'filled' : ''}
+              required
+            />
+            <Form.Label className={formData.Phone ? 'filled' : ''}>Teléfono</Form.Label>
+            {phoneError && <span className="text-danger">{phoneError}</span>}
+          </Form.Group>
+
+          <Form.Group controlId="formAvatar" className="mb-3">
+            <Form.Label>Avatar</Form.Label>
+            {formData.Avatar_URL && (
+              <div className="mb-2">
+                <Image
+                  src={formData.Avatar_URL}
+                  alt="Avatar"
+                  roundedCircle
+                  width={100}
+                  height={100}
+                />
+              </div>
+            )}
+            <Form.Control
+              type="file"
+              name="Avatar_File"
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Guardar cambios
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
+};
+
+export default PerfilUsuario;
