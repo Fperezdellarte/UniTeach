4f234cdfa8c2e02a1821b987d787494884de21bd
Alvaro <alvaro.ybanez16@gmail.com>
2025-01-24 20:34:36 -0300

    arreglo visual rapido en cards

--- src/index.js
+++ src/index.js
@@ -1,49 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider,Outlet } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Login from './routes/login';
-import Signup from './routes/signup';
-import About from './routes/about';
-import HomeLogueado from './routes/homeLogueado';
-import Results from './routes/Results';
-import  PerfilMentor from './routes/perfilMentor';
-import LandingPage from './routes/landingPage';
-import PerfilUsuario from './routes/perfilUsuario';
-import './styles/index.css';
-import { AuthProvider } from './auth/authProvider';
-import { ClassesProvider } from './contexts/classesContext'; // Importa el ClassesProvider
-import ProtectedRoute from './routes/protectedRoute';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from '../src/components/footer';
-import Clases from '../src/routes/clases'
-import ResetPassword from './routes/Reset-Password';
-import EmailForm  from './routes/emailForm';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Login from "./routes/login";
+import Signup from "./routes/signup";
+import About from "./routes/about";
+import HomeLogueado from "./routes/homeLogueado";
+import Results from "./routes/Results";
+import PerfilMentor from "./routes/perfilMentor";
+import LandingPage from "./routes/landingPage";
+import PerfilUsuario from "./routes/perfilUsuario";
+import "./styles/index.css";
+import { AuthProvider } from "./auth/authProvider";
+import { ClassesProvider } from "./contexts/classesContext"; // Importa el ClassesProvider
+import ProtectedRoute from "./routes/protectedRoute";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "../src/components/footer";
+import Clases from "../src/routes/clases";
+import ResetPassword from "./routes/Reset-Password";
+import EmailForm from "./routes/emailForm";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LandingPage />
+    element: <LandingPage />,
   },
   {
     path: "/signup",
-    element: <Signup />
+    element: <Signup />,
   },
   {
     path: "/login",
-    element: <Login />
+    element: <Login />,
   },
   {
     path: "/about",
-    element: <About />
+    element: <About />,
   },
   {
     path: "/reset-password/:token",
-    element:<ResetPassword/>
+    element: <ResetPassword />,
   },
-  { 
-    path: "/email-form", 
-    element:<EmailForm />
+  {
+    path: "/email-form",
+    element: <EmailForm />,
   },
   {
     path: "/",
@@ -56,36 +56,34 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/home",
-        element: <HomeLogueado />
+        element: <HomeLogueado />,
       },
       {
-        path:"/perfil",
-        element: <PerfilUsuario />
+        path: "/perfil",
+        element: <PerfilUsuario />,
       },
       {
         path: "/results",
-        element: <Results />
+        element: <Results />,
       },
       {
         path: "/perfilMentor/:id",
-        element: <PerfilMentor/>
+        element: <PerfilMentor />,
       },
       {
         path: "/clases",
-        element: <Clases />
-      }
-    ]
-  }
+        element: <Clases />,
+      },
+    ],
+  },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
     <ClassesProvider>
       <RouterProvider router={router} />
-          <Footer />
+      <Footer />
     </ClassesProvider>
   </AuthProvider>
 );
-
-
--- src/routes/about.js
+++ src/routes/about.js
@@ -1,74 +1,102 @@
-  import React from 'react';
-  import Navbar from '../components/navbar';
-  import '../styles/about.css';
-  import { Card, Row, Col, Button } from 'react-bootstrap';
+import React from "react";
+import Navbar from "../components/navbar";
+import "../styles/about.css";
+import { Card, Row, Col, Button } from "react-bootstrap";
 
-  const TestimonialCard = ({ name, quote }) => {
-    return (
-      <Card className="testimonial-card">
-        <Card.Body className="card-body">
-          <Card.Title className="card-title">{name}</Card.Title>
-          <Card.Text className="lead font-italic">{quote}</Card.Text>
-        </Card.Body>
-      </Card>
-    );
-  };
-  const handleFeedbackClick = () => {
-    const recipient = "uniteach24@gmail.com";
-    const subject = encodeURIComponent("Comentario sobre uniteach aplicación");
-    const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${recipient}&su=${subject}`;
+const TestimonialCard = ({ name, quote }) => {
+  return (
+    <Card className="testimonial-card">
+      <Card.Body className="card-body">
+        <Card.Title className="card-title">{name}</Card.Title>
+        <Card.Text className="lead font-italic">{quote}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+const handleFeedbackClick = () => {
+  const recipient = "uniteach24@gmail.com";
+  const subject = encodeURIComponent("Comentario sobre uniteach aplicación");
+  const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${recipient}&su=${subject}`;
 
-    window.open(gmailLink, "_blank");
-  };
+  window.open(gmailLink, "_blank");
+};
 
-  const TestimonialSection = () => {
-    const testimonials = [
-      { name: "Juan Pérez", quote: "Gracias a Uniteach, he mejorado mis habilidades en matemáticas y ahora estoy más confiado para mis exámenes." },
-      { name: "María Gutiérrez", quote: "Encontré a un excelente mentor de programación que me ayudó a entender conceptos difíciles de una manera clara y sencilla." },
-      { name: "Carlos Paez", quote: "Una de las mejores herramientas para el aprendizaje de manera versátil." },
-    ];
+const TestimonialSection = () => {
+  const testimonials = [
+    {
+      name: "Juan Pérez",
+      quote:
+        "Gracias a Uniteach, he mejorado mis habilidades en matemáticas y ahora estoy más confiado para mis exámenes.",
+    },
+    {
+      name: "María Gutiérrez",
+      quote:
+        "Encontré a un excelente mentor de programación que me ayudó a entender conceptos difíciles de una manera clara y sencilla.",
+    },
+    {
+      name: "Carlos Paez",
+      quote:
+        "Una de las mejores herramientas para el aprendizaje de manera versátil.",
+    },
+  ];
 
-    return (
-      <div className="testimonial-section">
-        <h2 className="text-center text-black">Testimonios de Usuarios</h2>
-        <Row className="testimonial-card-container justify-content-center">
-          {testimonials.map((testimonial, index) => (
-            <Col key={index} xs={12} md={4} className="d-flex justify-content-center">
-              <TestimonialCard {...testimonial} />
-            </Col>
-          ))}
+  return (
+    <div className="testimonial-section">
+      <h2 className="text-center text-black">Testimonios de Usuarios</h2>
+      <Row className="testimonial-card-container justify-content-center">
+        {testimonials.map((testimonial, index) => (
+          <Col
+            key={index}
+            xs={12}
+            md={4}
+            className="d-flex justify-content-center"
+          >
+            <TestimonialCard {...testimonial} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="container-about">
+        <Row>
+          {/* Columna para "Acerca de Nosotros" */}
+          <Col md={6}>
+            <div className="about fw-bold">
+              <h2 className="text-white">Acerca de Nosotros</h2>
+              <p className="text-white">
+                Somos Uniteach, una plataforma creada por estudiantes que
+                conecta a otros estudiantes universitarios para ofrecer y
+                recibir mentorías académicas.
+              </p>
+              <p className="text-white">
+                Nuestra misión es facilitar el intercambio de conocimientos y
+                promover el éxito académico de nuestros usuarios.
+              </p>
+            </div>
+          </Col>
+
+          {/* Columna para "Contacto" */}
+          <Col md={6}>
+            <div className="contact fw-bold">
+              <h2 className="text-white">Contacto</h2>
+              <p className="text-white">
+                ¿Tienes alguna pregunta o comentario? ¡Contáctanos!
+              </p>
+              <Button onClick={handleFeedbackClick} className="blue-button">
+                Enviar Mensaje
+              </Button>
+            </div>
+          </Col>
         </Row>
       </div>
-    );
-  };
-
-  export const About = () => {
-    return (
-      <div> 
-        <Navbar/>    
-        <div className='container-about'>
-          <Row>
-            {/* Columna para "Acerca de Nosotros" */}
-            <Col md={6}>
-              <div className='about fw-bold'>
-                <h2 className='text-white'>Acerca de Nosotros</h2>
-                <p className='text-white'>Somos Uniteach, una plataforma creada por estudiantes que conecta a otros estudiantes universitarios para ofrecer y recibir mentorías académicas.</p>
-                <p className='text-white'>Nuestra misión es facilitar el intercambio de conocimientos y promover el éxito académico de nuestros usuarios.</p>
-              </div>
-            </Col>
-
-            {/* Columna para "Contacto" */}
-            <Col md={6}>
-              <div className="contact fw-bold">
-                <h2 className='text-white'>Contacto</h2>
-                <p className='text-white'>¿Tienes alguna pregunta o comentario? ¡Contáctanos!</p>
-                  <Button onClick={handleFeedbackClick} className="blue-button">Enviar Mensaje</Button>
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default About;
